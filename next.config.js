da90d202033252118985f1e10f1271aec0c8d0f6
Tramcnb1804 <tramcung622@gmail.com>
2026-02-02 16:31:01 +0700

    up

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    // Cho phép import file mp3 như module (dùng cho nhạc nền)
+    config.module.rules.push({
+      test: /\.mp3$/i,
+      type: 'asset/resource',
+    })
+    return config
+  },
+}
 
 module.exports = nextConfig
